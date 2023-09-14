100ea0aa17d3f47e89fcd86942d15fff8a433b73
CinziaPulvirenti <cinzia2092@hotmail.it>
2023-09-14 20:00:36 +0200

    Aggiungere i file di progetto.

---
+++ Address.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public class Address
+    {
+        public string Name {  get; set; }
+        public string Surname {  get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string ZIP { get; set; }
+
+
+        public Address(string name, string surname, string street, string city, string province, string zip)
+        { 
+            Name = name;
+            Surname = surname;
+            Street = street;
+            City = city;
+            Province = province;
+            ZIP = zip;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Nome {this.Name} cognome {this.Surname} via {this.Street} città {this.City} CAP {this.ZIP}";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace csharp_lista_indirizzi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Address> addresses = new List<Address>();
+
+            try
+            {
+                StreamReader fileAddress = File.OpenText("C:\\Users\\Cinzia\\source\\repos\\csharp-lista-indirizzi\\my-addresses.csv");
+
+                int lineNumber = 0;
+
+                while (!fileAddress.EndOfStream)
+                {
+                    string line = fileAddress.ReadLine();
+
+                    lineNumber++;
+
+                    if (lineNumber > 1)
+                    {
+
+                        string[] stringSplits = line.Split(',');
+
+                        if(stringSplits.Length == 6)
+                        {
+                            string name = stringSplits[0].TrimStart(' ');
+                            string surname = stringSplits[1];
+                            string street = stringSplits[2];
+                            string city = stringSplits[3];
+                            string province = stringSplits[4];
+                            string ZIP = stringSplits[5];
+
+                            Address newAddress = new Address(name, surname, street, city, province, ZIP);
+
+                            Console.WriteLine(newAddress.ToString());
+                        }
+                        else
+                        {
+                            Console.WriteLine("Indirizzo non valido");
+                        }
+                    }
+                }
+
+                fileAddress.Close();
+            } catch
+            {
+                Console.WriteLine("Ops! Qualcosa è andato storto! :(");
+            }
+        }
+    }
+}
